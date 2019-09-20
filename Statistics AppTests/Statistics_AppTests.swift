c210877473d3bb1e38ee1c1238d3bb78c052e154
Abhi Shukul <abhishukul16@gmail.com>
2019-09-20 18:46:30 -0400

    Initial Commit

---
+++ Statistics AppTests/Statistics_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Statistics_AppTests.swift
+//  Statistics AppTests
+//
+//  Created by Abhi Shukul on 9/20/19.
+//  Copyright © 2019 Abhi Shukul. All rights reserved.
+//
+
+import XCTest
+@testable import Statistics_App
+
+class Statistics_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
